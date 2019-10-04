ab14ebc315d29e49074f9977595894b3c08a4bf6
Nicolas Emanuel Morales <nicolasemanuelmorales@hotmail.com>
2019-10-03 23:22:46 -0300

    agregadod e vista login y registrar, con validaciones

--- ProyectoPW3-AyudandoAlProjimo/Controllers/HomeController.cs
+++ ProyectoPW3-AyudandoAlProjimo/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace ProyectoPW3_AyudandoAlProjimo.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ ProyectoPW3-AyudandoAlProjimo/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoPW3_AyudandoAlProjimo.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ValidarUsuario(Usuario u)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction("Index","Home");
+            }
+            return View("Login");
+
+        }
+        [HttpPost]
+        public ActionResult ValidarUsuarioRegistrar(Usuario u)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Registrar");
+
+        }
+
+    }
+}
