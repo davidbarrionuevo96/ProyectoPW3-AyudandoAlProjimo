c533f3aa19035f015492c2598039ec944f84a1ec
Nicolas Emanuel Morales <nicolasemanuelmorales@hotmail.com>
2019-10-13 02:25:27 -0300

    validacion e integracion de bd

--- ProyectoPW3-AyudandoAlProjimo/Controllers/LoginController.cs
+++ ProyectoPW3-AyudandoAlProjimo/Controllers/LoginController.cs
@@ -9,6 +9,7 @@ namespace ProyectoPW3_AyudandoAlProjimo.Controllers
 {
     public class LoginController : Controller
     {
+        Entities asd = new Entities();
         // GET: Login
         public ActionResult Login()
         {
@@ -21,21 +22,46 @@ namespace ProyectoPW3_AyudandoAlProjimo.Controllers
         }
 
         [HttpPost]
-        public ActionResult ValidarUsuario(Usuario u)
+        public ActionResult ValidarUsuario(Usuarios u)
         {
             if (ModelState.IsValid)
             {
-
                 return RedirectToAction("Index","Home");
             }
             return View("Login");
 
         }
         [HttpPost]
-        public ActionResult ValidarUsuarioRegistrar(Usuario u)
+        public ActionResult ValidarUsuarioRegistrar(Usuarios u, FormCollection s)
         {
+            if (s["pass"].Length == 0)
+            {
+                ModelState.AddModelError("pass", "puto el que lee 2.");
+
+
+
+            }
+           else if (!u.Password.Equals(s["pass"]))
+            {
+                ModelState.AddModelError("pass", "puto el que lee.");
+            }
+
+            
+
             if (ModelState.IsValid)
             {
+                Usuarios n = new Usuarios();
+                
+                n.Activo = true;
+                n.Token = "asdsad";
+                n.FechaCracion = DateTime.Today;
+                n.TipoUsuario = 1;
+                n.FechaNacimiento = u.FechaNacimiento;
+                n.Email = u.Email;
+                n.Password = u.Password;
+                
+                asd.Usuarios.Add(n);
+                asd.SaveChanges();
 
                 return RedirectToAction("Index", "Home");
             }
