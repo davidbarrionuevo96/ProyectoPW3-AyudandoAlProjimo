35d22863a4c55deb9857a6077a926fb130321c85
ibarrionuevo50 <david.barrionuevo96@hotmail.com>
2019-10-21 17:22:53 -0300

    Vista completa falta foto y logica de elegir en controlador

---
+++ Servicios/PropuestaService.cs
@@ -0,0 +1,26 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class PropuestaService
+    {
+        public void RegistrarPropuesta(Propuestas p)
+        {
+            using (Entities ctx = new Entities())
+            {
+
+                p.FechaCreacion = DateTime.Today;
+                p.Estado = 1;
+                p.Foto = "asdas";
+                ctx.Propuestas.Add(p);
+                ctx.SaveChanges();
+            }
+        }
+        
+    }
+}
