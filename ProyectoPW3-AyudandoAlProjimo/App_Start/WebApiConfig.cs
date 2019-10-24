ee8357984843e120ddad60315dd85127abb25166
ibarrionuevo50 <david.barrionuevo96@hotmail.com>
2019-10-24 17:06:07 -0300

    WebApi and leer json

--- ProyectoPW3-AyudandoAlProjimo/App_Start/WebApiConfig.cs
+++ ProyectoPW3-AyudandoAlProjimo/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace WebApiDonaciones
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
         }
     }
 }
