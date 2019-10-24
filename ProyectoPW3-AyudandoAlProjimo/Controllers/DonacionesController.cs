955063a2d5b1232d6fee2aae63770063f5d46b67
ibarrionuevo50 <david.barrionuevo96@hotmail.com>
2019-10-24 16:37:07 -0300

    WebApi conectada a proyecto

---
+++ ProyectoPW3-AyudandoAlProjimo/Controllers/DonacionesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entidades;
+using Servicios;
+using WebApiDonaciones.Controllers;
+namespace ProyectoPW3_AyudandoAlProjimo.Controllers
+{
+    public class DonacionesController : Controller
+    {
+        
+        public ActionResult MiHistorialDonaciones()
+        {
+            
+            return View();
+        }
+    }
+}
--- ProyectoPW3-AyudandoAlProjimo/Global.asax.cs
+++ ProyectoPW3-AyudandoAlProjimo/Global.asax.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApiDonaciones;
 
 namespace ProyectoPW3_AyudandoAlProjimo
 {
@@ -14,6 +16,9 @@ namespace ProyectoPW3_AyudandoAlProjimo
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+#pragma warning disable CS0436 // El tipo entra en conflicto con un tipo importado
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+#pragma warning restore CS0436 // El tipo entra en conflicto con un tipo importado
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
